f5f09d18bc052b07988cfb42c8e8042cbb7d0d98
evanyang0612 <blacksheet@gmail.com>
2022-11-05 19:05:47 +0800

    update

--- src/pages/Favoirte/Favorite.js
+++ src/pages/Favoirte/Favorite.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { Context } from "../../Context";
 import FavoriteItem from "../../components/FavoriteItem/FavoriteItem";
 import "./Favorite.css";
